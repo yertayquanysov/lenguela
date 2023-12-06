0ace73e759ac27c2200db167ecc57013247c3155
ertai <ertai.master@gmail.com>
2023-12-06 11:46:45 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lenguela/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lenguela
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lenguela/features/dashboard/presentation/pages/dashboard_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: GoogleFonts.ubuntuTextTheme(),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.redAccent,
+        ),
+      ),
+      home: const DashboardPage(),
+    );
+  }
+}
